d1613023586b16a9e8ecf86dbd58b5e9c0e3d3c9
nacm1269 <nacmuran@gmail.com>
2021-06-14 14:46:31 +0100

    created architecture and function to retrieve starter item json object

---
+++ src/features/MenuPage/MenuCourses/MenuCourses.js
@@ -0,0 +1,1 @@
+import StartersNav from './StartersNav/StartersNav'
---
+++ src/features/MenuPage/MenuCourses/StartersNav/StartersNav.js
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+
+const StartersNav = () => {
+    const [starters, setStarters] = useState([])
+    const getStarters = async () => {
+        const data = await fetch()
+        return await data.json()
+    }
+    useEffect(() => {
+        getStarters()
+            .then(json => {
+                if (json.success) {
+                    setStarters(json.data.starters)
+                }
+            })
+    })
+    const displayStarters = () => {
+        if (starters.length > 0) {
+            return starters.map(starter => {
+                return <StarterItem key={starter._id} name={starter.name} description={starter.description}
+                                    price={starter.price}></StarterItem>
+            })
+        }
+        else return null
+    }
+    return (
+        <main>
+            {displayStarters()}
+        </main>
+    )
+
+}
+
+export default StartersNav
