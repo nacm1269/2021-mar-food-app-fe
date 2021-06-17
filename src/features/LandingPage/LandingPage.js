fa4b618eea7346be7014e5670f383bef093a0674
SteeleSR <samuelrsteele@gmail.com>
2021-06-18 00:39:41 +0100

    changed logo

--- src/features/LandingPage/LandingPage.js
+++ src/features/LandingPage/LandingPage.js
@@ -4,25 +4,24 @@ import { Button} from 'react-materialize';
 import {Link} from 'react-router-dom'
 import restaurantImage from './restaurantImage.jpg'
 import balogo1 from './balogo1.png'
-import whiteBackground from './whiteBackground.png'
+import bonLogo from '../../Bon-logo.png'
 import './LandingPage.css'
 
 const LandingPage = () => {
     return (
         <div className='responsive-img box'
              style={{
-                 backgroundImage: `url(${whiteBackground})`,
-                 backgroundSize: "contain",
+                 backgroundImage: `url(${restaurantImage})`,
+                 backgroundSize: "cover",
                  height: "100vh",
                  width: "100vw",
                  alt: "restaurant hanging lights",
-                 backgroundBlendMode: "multiply"
              }}>
             <div>
                 <img className={'responsive-img'} alt='logo' style={{
                     width: "50vh",
                     alt: "bon appétit logo"
-                }} src={balogo1}/>
+                }} src={bonLogo}/>
                 <h5 className={'landing-tagline'}>Michelin-starred food delivered to your door</h5>
             </div>
             <div style={{
